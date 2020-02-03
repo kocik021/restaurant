ab0f6f5a05f36b959e9b163b3b5e054e201cbd30
Jarek Kocik <kocik1908@gmail.com>
2020-02-03 21:40:26 +0000

    Updates

---
+++ precache-manifest.85cf9cdc46a35510b473ef42493413d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bf2dac718e4235fc8028b1f96a805a",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "29f9082d7a1a74d50f0c",
+    "url": "/restaurant/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "53715d09f65592049c8a",
+    "url": "/restaurant/static/css/main.f7299935.chunk.css"
+  },
+  {
+    "revision": "29f9082d7a1a74d50f0c",
+    "url": "/restaurant/static/js/2.033c41a6.chunk.js"
+  },
+  {
+    "revision": "b2a031b4f2fcca5840ab1535819b3db6",
+    "url": "/restaurant/static/js/2.033c41a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53715d09f65592049c8a",
+    "url": "/restaurant/static/js/main.c5b4605e.chunk.js"
+  },
+  {
+    "revision": "52fc41dea76ca30e6f96",
+    "url": "/restaurant/static/js/runtime-main.abd73592.js"
+  },
+  {
+    "revision": "03ebe9c8feb77cdaa8afa07a2ac6fa8c",
+    "url": "/restaurant/static/media/restaurant-gallery.03ebe9c8.jpg"
+  }
+]);
